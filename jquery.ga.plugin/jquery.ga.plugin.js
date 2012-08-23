f2f9cceeff7b17284172f5a4d3c8043cc8ad3ca0
Ignatius Hsu <ignatiushsu@gmail.com>
2012-08-23 03:21:45 -0400

    Partial fix to race condition bug. Addressed through addition of .preventDefault();
    Addresses bug for "Outbound link tracking affected by race condition
    between href and js onclick event"
    https://github.com/ignatiushsu/google-analytics-plugin/issues/1
    
    Still needs work and testing. Possibly requires addition of
    setTimeout().
    
    Thank you Jonathan Weber (@jonathanweber) for pointing out the bug in
    the first place, and for the jQuery API solution.

--- jquery.ga.plugin/jquery.ga.plugin.js
+++ jquery.ga.plugin/jquery.ga.plugin.js
@@ -35,46 +35,45 @@ $(document).ready(function() {
 	// X-DOMAIN
 	// track links as x-domain if matches x-domain list + does not match current host + does not match downloaded files.
 	// does it need to exclude all hosts matching second level domain (blog.foo.com host matches foo.com)???
-	$('a').filter($ga_xdomain_fn).not(function() {
+	$('a').filter($ga_xdomain_fn).not(function(e) {
 		//exclude current SLD.TLD
 		var ga_xdomain_current = new RegExp('^https?:\/\/[^\/]*?' + $ga_sld_tld + '|' + $ga_file, "i");
 		return this.href.match(ga_xdomain_current);
 	})
-	.click(function() {
-		//_gaq.push(['_link', this.href.toLowerCase()]);
+	.click(function(e) {
 		alert('Cross Domain Link Tracking Test\n Full Href: ' + this.href + '\n GA Value: ' + this.href.toLowerCase() + '\n This SLD.TLD: ' + $ga_sld_tld );
-		return false;
+		_gaq.push(['_link', this.href.toLowerCase()]);
 	});
 	
 	// OUTBOUND
-	$('a').not(function() {
+	$('a').not(function(e) {
 		var ga_outbound_fn = new RegExp('^$|mailto:|' + $ga_xdomain + '|' + $ga_file, "i");
 		return this.href.match(ga_outbound_fn);
 	})
-	.click(function(event) {
+	.click(function(e) {
+		e.preventDefault();
 		var ga_trk_outbound = this.href.match(/\/\/([^\/]+)/i)[1].toLowerCase();
 		_gaq.push(['_trackEvent', 'outbound', 'click', ga_trk_outbound]);
 		_gaq.push(['master._trackEvent', 'outbound', 'click', ga_trk_outbound]);
 		alert('Outbound Test\n Full Href: ' + this.href + '\n GA Value: ' + ga_trk_outbound);
-		return false;
 	});
 
 	// DOWNLOADS
-	$('a').filter($ga_file_fn).click(function() {
+	$('a').filter($ga_file_fn).click(function(e) {
+		e.preventDefault();
 		var ga_trk_download = this.href.replace(/^https?:\/\//, '').toLowerCase();
 		_gaq.push(['_trackEvent', 'download', 'click', ga_trk_download]);
 		_gaq.push(['master._trackEvent', 'download', 'click', ga_trk_download]);
-		 alert('Download Test\n Full Href: ' + this.href + '\n GA Value: ' + ga_trk_download);
-		return false;
+		alert('Download Test\n Full Href: ' + this.href + '\n GA Value: ' + ga_trk_download);
 	});
 
 	// MAILTO
-	$('a[href^="mailto:"],a[href^="Mailto:"],a[href^="MAILTO:"]').click(function() {
+	$('a[href^="mailto:"],a[href^="Mailto:"],a[href^="MAILTO:"]').click(function(e) {
+		e.preventDefault();
 		var ga_trk_mailto = this.href.replace(/^mailto:/i, '').toLowerCase();
 		_gaq.push(['_trackSocial', 'email', 'send', ga_trk_mailto]);
 		_gaq.push(['master._trackSocial', 'email', 'send', ga_trk_mailto]);
 		alert('Mailto Test\n Full Href: ' + this.href + '\n GA Value: ' + ga_trk_mailto);
-		return false;
 	});
 	
 	// 404 ERROR PAGES

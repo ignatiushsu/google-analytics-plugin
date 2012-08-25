3186914e50d9d6ee699b24671aa8fee1a8dd19bc
Ignatius Hsu <ignatiushsu@gmail.com>
2012-08-25 14:39:59 -0400

    Race condition bug fix - redirect URLs
    Previous fix prevents default click action.
    
    Final action in functions will redirect users to URL attached to the
    click event. Use window.location = this.href, which retains browser
    history. This allows visitor to hit the back button to return to the
    original page. Alternate is window.location.replace(url) which replaces
    original URL in browser with redirect page, removing original URL from
    browser history. Visitor cannot hit back button to return to the
    original page.

--- jquery.ga.plugin/jquery.ga.plugin.js
+++ jquery.ga.plugin/jquery.ga.plugin.js
@@ -3,7 +3,7 @@ Georgetown University
 Google Analytics for Office of Advancement
 *************************/
 
-// Link tests+dev notes on Dropbox 
+// Link tests+dev notes on Dropbox
 
 /********** jQuery Google Analytics Async Enhancements **********/
 /* 
@@ -35,18 +35,19 @@ $(document).ready(function() {
 	// X-DOMAIN
 	// track links as x-domain if matches x-domain list + does not match current host + does not match downloaded files.
 	// does it need to exclude all hosts matching second level domain (blog.foo.com host matches foo.com)???
-	$('a').filter($ga_xdomain_fn).not(function(e) {
+	$('a').filter($ga_xdomain_fn).not(function() {
 		//exclude current SLD.TLD
 		var ga_xdomain_current = new RegExp('^https?:\/\/[^\/]*?' + $ga_sld_tld + '|' + $ga_file, "i");
 		return this.href.match(ga_xdomain_current);
 	})
 	.click(function(e) {
-		alert('Cross Domain Link Tracking Test\n Full Href: ' + this.href + '\n GA Value: ' + this.href.toLowerCase() + '\n This SLD.TLD: ' + $ga_sld_tld );
+		e.preventDefault();
 		_gaq.push(['_link', this.href.toLowerCase()]);
+		alert('Cross Domain Link Tracking Test\n Full Href: ' + this.href + '\n GA Value: ' + this.href.toLowerCase() + '\n This SLD.TLD: ' + $ga_sld_tld );
 	});
 	
 	// OUTBOUND
-	$('a').not(function(e) {
+	$('a').not(function() {
 		var ga_outbound_fn = new RegExp('^$|mailto:|' + $ga_xdomain + '|' + $ga_file, "i");
 		return this.href.match(ga_outbound_fn);
 	})
@@ -56,6 +57,7 @@ $(document).ready(function() {
 		_gaq.push(['_trackEvent', 'outbound', 'click', ga_trk_outbound]);
 		_gaq.push(['master._trackEvent', 'outbound', 'click', ga_trk_outbound]);
 		alert('Outbound Test\n Full Href: ' + this.href + '\n GA Value: ' + ga_trk_outbound);
+		window.location = this.href;
 	});
 
 	// DOWNLOADS
@@ -65,6 +67,7 @@ $(document).ready(function() {
 		_gaq.push(['_trackEvent', 'download', 'click', ga_trk_download]);
 		_gaq.push(['master._trackEvent', 'download', 'click', ga_trk_download]);
 		alert('Download Test\n Full Href: ' + this.href + '\n GA Value: ' + ga_trk_download);
+		window.location = this.href;
 	});
 
 	// MAILTO
@@ -74,6 +77,7 @@ $(document).ready(function() {
 		_gaq.push(['_trackSocial', 'email', 'send', ga_trk_mailto]);
 		_gaq.push(['master._trackSocial', 'email', 'send', ga_trk_mailto]);
 		alert('Mailto Test\n Full Href: ' + this.href + '\n GA Value: ' + ga_trk_mailto);
+		window.location = this.href;
 	});
 	
 	// 404 ERROR PAGES

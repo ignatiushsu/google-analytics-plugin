6987f9daebdddac2d2f1791eb7cf1576d272c345
Ignatius Hsu <ignatiushsu@gmail.com>
2013-03-16 03:19:06 -0400

    Updated minor version number to v1.1.02

--- jquery.ga.plugin/jquery.ga.plugin.js
+++ jquery.ga.plugin/jquery.ga.plugin.js
@@ -2,7 +2,7 @@
 
 /********** jQuery Google Analytics Async Enhancements **********/
 /* 
- * v1.1.01 new function for tracking domain redirects. Requires jQuery 1.4.2+ and GA async. Read the change log + developer notes.
+ * v1.1.02 new function for tracking domain redirects. Requires jQuery 1.4.2+ and GA async. Read the change log + developer notes.
  * Developed by Ignatius Hsu, Copyright 2012 Georgetown University. Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0) http://creativecommons.org/licenses/by-nc-sa/3.0/ and is provided as is, without guarantee or support.
  * Attribution: This code is inspired by gaAddons FREE v1.0, Copyright 2011 Stéphane Hamel (http://gaAddons.com), licensed under CC BY-NC-SA 3.0.
  */
